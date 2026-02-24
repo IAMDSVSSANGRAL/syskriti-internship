523eeb097f574723e073cea4cac979cbc855f267
IAMDSVSSANGRAL <havinosh.india@gmail.com>
2026-02-24 09:15:16 +0530

    taskpy200_t2

---
+++ TaskPY200_T2.py
@@ -0,0 +1,10 @@
+# Take input from user
+num = float(input("Enter a number: "))
+
+# Check condition
+if num > 0:
+    print(f"Your entered number {num} was positive.")
+elif num < 0:
+    print(f"Your entered number {num} was negative, however positive value of your number is {abs(num)}.")
+else:
+    print(f"Your entered number {num} was zero.")
